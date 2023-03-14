39162d65716cbec41d07370344daa42e05a746f1
Oleksandr Ovrakh <oleksandr.ovrakh@gmail.com>
2023-03-14 23:09:20 +0200

    fix

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,14 @@ const button = document.querySelector("button")!;
 const input1 = document.getElementById("num1")! as HTMLInputElement;
 const input2 = document.getElementById("num2")! as HTMLInputElement;
 
+function add(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+button.addEventListener("click", function () {
+  console.log(add(+input1.value, +input2.value));
+});
+
 // let age: number;
 // age = 50;
 // let nam: string;
